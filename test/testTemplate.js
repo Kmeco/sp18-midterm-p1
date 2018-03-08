a9d8978fa7a53396e58194f37ded92a25ce5c389
Jakub Kmec <jakub.kmec@berkeley.edu>
2018-03-08 12:03:46 -0800

    minor change

--- test/testTemplate.js
+++ test/testTemplate.js
@@ -2,28 +2,32 @@
 
 /* Add the dependencies you're testing */
 const Crowdsale = artifacts.require("./Crowdsale.sol");
+const Queue = artifacts.require("./Queue.sol");
+const Token = artifacts.require("./Token.sol");
 // YOUR CODE HERE
 
-contract('testTemplate', function(accounts) {
+contract('testICO', function(accounts) {
 	/* Define your constant variables and instantiate constantly changing 
 	 * ones
 	 */
-	const args = {};
-	let x, y, z;
+	const args = {_duration : 10000, _timeLimit : 1000, _totalSupply : 1000000};
+	let myToken, crowdsale, queue;
 	// YOUR CODE HERE
 
 	/* Do something before every `describe` method */
 	beforeEach(async function() {
 		// YOUR CODE HERE
+        crowdsale = await Crowdsale.new(args._totalSupply, args._duration);
+        queue = await Queue.new(now, args._timeLimit)
 	});
 
 	/* Group test cases together 
 	 * Make sure to provide descriptive strings for method arguements and
 	 * assert statements
 	 */
-	describe('Your string here', function() {
-		it("your string here", async function() {
-			// YOUR CODE HERE
+	describe('--Queue works--', function() {
+		it("Returns the number of people waiting in line", async function() {
+			
 		});
 		// YOUR CODE HERE
 	});
